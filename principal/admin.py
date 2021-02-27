65d9ef2b1c37f0e5ac399fe9904b909ef9d7694a
clinicmebsoft <armartinezt@gmail.com>
2021-02-27 00:14:58 -0600

    21 febrero personal

--- principal/admin.py
+++ principal/admin.py
@@ -1,8 +1,16 @@
 
 from django.contrib import admin
 from pacientes.models import *
+from inventarios.models import *
 # Register your models here.
 
 
 admin.site.register(Paciente)
 admin.site.register(HistoriaClinica)
+admin.site.register(Articulos)
+admin.site.register(Impuestos)
+admin.site.register(Proveedores)
+
+
+
+
